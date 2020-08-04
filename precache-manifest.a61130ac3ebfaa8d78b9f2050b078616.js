566f11076b8c8ed3fbbf44f7fb72d23f3a1ffb98
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-04 22:18:42 +0800

    Updates

---
+++ precache-manifest.a61130ac3ebfaa8d78b9f2050b078616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482f66035fd74b586c0ffe72072edc96",
+    "url": "/resort-ui/index.html"
+  },
+  {
+    "revision": "88a6a19e22874286f386",
+    "url": "/resort-ui/static/css/main.80530b71.chunk.css"
+  },
+  {
+    "revision": "a5736519d30c56196490",
+    "url": "/resort-ui/static/js/2.0683570b.chunk.js"
+  },
+  {
+    "revision": "c8cb351001e5261fe136ce260d22acbe",
+    "url": "/resort-ui/static/js/2.0683570b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a6a19e22874286f386",
+    "url": "/resort-ui/static/js/main.8f8a2c0a.chunk.js"
+  },
+  {
+    "revision": "d769706ac5a2cc5779c2",
+    "url": "/resort-ui/static/js/runtime-main.6af300f1.js"
+  },
+  {
+    "revision": "f34bbfc0f628b73d14c4a3f1dd5cb791",
+    "url": "/resort-ui/static/media/resort.f34bbfc0.jpg"
+  }
+]);
